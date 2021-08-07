bd66571721b09e879cd577b993d4c6db56629721
rajnil-guha01 <rajnil94.guha@gmail.com>
2021-08-07 17:46:19 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="iht-profiling-beam-summit-2021",
+    name="beam-avro-testing",
     version=0.1,
     packages=setuptools.find_packages(),
     install_requires=['python-snappy', 'apache-beam[gcp]'])
