efc3f369c63bdd2a38b88eacdfa8a4c2e70816ce
rajnil-guha01 <rajnil94.guha@gmail.com>
2021-08-25 01:02:56 +0530

    Added commands for dataflow flex-template

--- scripts_and_notes/run.sh
+++ scripts_and_notes/run.sh
@@ -1,10 +1,10 @@
 export PROJECT_ID=<>
 export RUNNER=DataflowRunner
-export TEMP_LOCATION=gsutil mb gs://dataflow-projects/tmp
-export STAGING_LOCATION=gsutil mb gs://dataflow-projects/tmp
+export TEMP_LOCATION=gs://dataflow-projects/tmp
+export STAGING_LOCATION=gs://dataflow-projects/tmp
 export REGION=us-central1
 export INPUT_FILE=<>
-export OUTPUT_BUCKET=gsutil mb gs://deadletter-sink/duplicate-records
+export OUTPUT_BUCKET=gs://deadletter-sink/duplicate-records
 export OUTPUT_TABLE=my_test_dataset.users_history
 
 python beam_avro.py --project=$PROJECT_ID \
